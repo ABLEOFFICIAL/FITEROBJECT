0420b687f3b28260a33e61035f5e7edbf2a789d3
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2024-11-14 10:57:46 -0800

    filter object learning

---
+++ script.js
@@ -0,0 +1,33 @@
+var searchBox = document.getElementById('searchBox');
+var contactnames = document.getElementById('contactnames');
+var contactList = document.querySelectorAll('.contactalphabets');
+
+
+// event listener
+searchBox.addEventListener('keyup', function() {
+    var inputValue = searchBox.value.toLowerCase();
+
+    for (let i = 0; i < contactList.length; i++) {
+      const contact = contactList[i];
+      const text = contact.textContent || contact.innerText; // Get the text content of the item
+      if (text.toLowerCase().indexOf(inputValue) > -1) {
+        contact.style.display = ''; // Show the item if it matches
+      } else {
+        contact.style.display = 'none'; // Hide the item if it doesn't match
+      }
+    }
+});
+
+contactList.forEach(function(contacts){
+    contacts.onclick = function(){
+        searchBox.innerHTML = this.textContent;
+    }
+});
+
+contactList.forEach(function(contact) {
+  contact.onclick = function() {
+      searchBox.innerHTML = this.textContent;
+      // banklist.classList.toggle('hide');
+      // arrow.classList.toggle('rotate');
+  };
+});
